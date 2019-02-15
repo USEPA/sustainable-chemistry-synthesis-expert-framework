95ccfc2e140cfb31ec87e3e8096918f9f3bbf57e
Bill Barrett <barrett.williamm@epa.gov>
2019-02-14 23:03:18 -0500

    Updated data path.

--- SustainableChemistryWeb/SustainableChemistryWeb/Controllers/HomeController.cs
+++ SustainableChemistryWeb/SustainableChemistryWeb/Controllers/HomeController.cs
@@ -24,8 +24,8 @@ namespace SustainableChemistryWeb.Controllers
             return View();
         }
 
-        // fg?smiles=O=P(OC)(OC)C
-        public async Task<IActionResult> FG(string smiles)
+        // Home/FunctionalGroups?smiles=O=P(OC)(OC)C
+        public async Task<IActionResult> FunctionalGroups(string smiles)
         {
             ChemInfo.Molecule molecule = new ChemInfo.Molecule(smiles);
             var fG = from f in _context.AppFunctionalgroup select f;
