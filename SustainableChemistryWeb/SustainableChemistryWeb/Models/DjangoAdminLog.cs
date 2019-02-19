35b07921e3b3051cfa6d40e7d142acf3097694cb
wbarret1 <barrett.williamm@epa.gov>
2019-02-19 17:09:02 -0500

    Fixed issue with data application static directories. Applied the EPA standalone app web template.

---
+++ SustainableChemistryWeb/SustainableChemistryWeb/Models/DjangoAdminLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SustainableChemistryWeb.Models
+{
+    public partial class DjangoAdminLog
+    {
+        public long Id { get; set; }
+        public string ActionTime { get; set; }
+        public string ObjectId { get; set; }
+        public string ObjectRepr { get; set; }
+        public string ChangeMessage { get; set; }
+        public long? ContentTypeId { get; set; }
+        public long UserId { get; set; }
+        public long ActionFlag { get; set; }
+
+        public virtual DjangoContentType ContentType { get; set; }
+        public virtual AuthUser User { get; set; }
+    }
+}
