dcfe8b961dd9a8de6aaca41a5b67e461d602bd90
wbarret1 <barrett.williamm@epa.gov>
2019-02-21 09:58:13 -0500

    Updated Data and Web apps.

--- SustainableChemistryWeb/SustainableChemistryWeb/Controllers/HomeController.cs
+++ SustainableChemistryWeb/SustainableChemistryWeb/Controllers/HomeController.cs
@@ -5,50 +5,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SustainableChemistryWeb.Models;
-using System.Text.Encodings.Web;
-using Microsoft.EntityFrameworkCore;
 
 namespace SustainableChemistryWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly SustainableChemistryContext _context;
-
-        public HomeController(SustainableChemistryContext context)
-        {
-            _context = context;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
-        // Home/FunctionalGroups?smiles=O=P(OC)(OC)C
-        public async Task<IActionResult> FunctionalGroups(string smiles)
-        {
-            ViewData["Smiles"] = smiles;
-            ChemInfo.Molecule molecule = new ChemInfo.Molecule(smiles);
-            var fG = await _context.AppFunctionalgroup.ToListAsync();
-            var retVal = new List<AppFunctionalgroupDTO>();
-            foreach (var group in fG)
-            {
-                string smarts = group.Smarts;
-                if (!string.IsNullOrEmpty(group.Smarts))
-                    if (molecule.FindFunctionalGroup(group.Smarts))
-                    {
-                        retVal.Add(new AppFunctionalgroupDTO()
-                        {
-                            Id = group.Id,
-                            Name = group.Name,
-                            Smarts = group.Smarts
-                        }
-                        );
-                    }
-            }
-            return View(retVal);
-        }
-
 
         public IActionResult Privacy()
         {
