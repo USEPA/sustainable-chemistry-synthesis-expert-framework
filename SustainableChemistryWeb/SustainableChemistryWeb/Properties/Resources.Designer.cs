117231bb2f53450a7e00b2131a00c77045e26e1f
wbarret1 <barrett.williamm@epa.gov>
2019-03-14 11:06:03 -0400

    Removed Blue Obelisk Data Repository files from ChemInfo folder. The element file was added as a string resource.

--- SustainableChemistryWeb/SustainableChemistryWeb/Properties/Resources.Designer.cs
+++ SustainableChemistryWeb/SustainableChemistryWeb/Properties/Resources.Designer.cs
@@ -73,25 +73,6 @@ namespace SustainableChemistryWeb.Properties {
         ///      xmlns:units=&quot;http://www.xml-cml.org/units/units&quot; 
         ///      xmlns:siUnits=&quot;http: [rest of string was truncated]&quot;;.
         /// </summary>
-        internal static string elements {
-            get {
-                return ResourceManager.GetString("elements", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
-        ///&lt;list id=&quot;chemicalElement&quot; convention=&quot;bodr:elements&quot;
-        ///      title=&quot;properties of the elements&quot;
-        ///
-        ///      xmlns=&quot;http://www.xml-cml.org/schema&quot; 
-        ///      xmlns:xml=&quot;http://www.w3.org/XML/1998/namespace&quot; 
-        ///      xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot; 
-        ///      xmlns:bo=&quot;http://www.blueobelisk.org/dict/terminology&quot; 
-        ///      xmlns:boUnits=&quot;http://www.blueobelisk.org/dict/units&quot; 
-        ///      xmlns:units=&quot;http://www.xml-cml.org/units/units&quot; 
-        ///      xmlns:siUnits=&quot;http: [rest of string was truncated]&quot;;.
-        /// </summary>
         internal static string elementString {
             get {
                 return ResourceManager.GetString("elementString", resourceCulture);
