f22bc5472a07b227a55508a7dc2488b9e13023d5
Bill Barrett <barrett.williamm@epa.gov>
2019-06-19 21:33:26 -0400

    Scaffolded solvents and catalysts.

---
+++ SustainableChemistryWeb/SustainableChemistryWeb/Controllers/SolventsController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SustainableChemistryWeb.Controllers
+{
+    public class SolventsController : Controller
+    {
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _hostingEnvironment;
+        private readonly SustainableChemistryWeb.Models.SustainableChemistryContext _context;
+
+        public SolventsController(SustainableChemistryWeb.Models.SustainableChemistryContext context, Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
+        {
+            _context = context;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET: Solvents
+        public ActionResult Index()
+        {
+            return View(_context.AppSolvent.ToList());
+        }
+
+        // GET: Solvents/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Solvents/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Solvents/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Solvents/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Solvents/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Solvents/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Solvents/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SustainableChemistryWeb/SustainableChemistryWeb/Pages/Solvents/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SustainableChemistryWeb.Models;
+
+namespace SustainableChemistryWeb.Views.Solvents
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SustainableChemistryWeb.Models.SustainableChemistryContext _context;
+
+        public DetailsModel(SustainableChemistryWeb.Models.SustainableChemistryContext context)
+        {
+            _context = context;
+        }
+
+        public Solvent Solvent { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Solvent = await _context.AppSolvent.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Solvent == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
