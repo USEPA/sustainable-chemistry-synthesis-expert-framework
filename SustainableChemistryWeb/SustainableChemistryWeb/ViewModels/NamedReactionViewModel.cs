714e5026d21ac36623a23c5393ef4aafe5858c0b
wbarret1 <barrett.williamm@epa.gov>
2019-07-09 08:20:27 -0400

    Made the Image not required. This allows Named reactions and Functional Groups to be edited without putting in a new image file.

--- SustainableChemistryWeb/SustainableChemistryWeb/ViewModels/NamedReactionViewModel.cs
+++ SustainableChemistryWeb/SustainableChemistryWeb/ViewModels/NamedReactionViewModel.cs
@@ -7,7 +7,6 @@ namespace SustainableChemistryWeb.ViewModels
 {
     public class NamedReactionViewModel: SustainableChemistryWeb.Models.NamedReaction
     {
-        [System.ComponentModel.DataAnnotations.Required]
         [System.ComponentModel.DisplayName("Image File")]
         public Microsoft.AspNetCore.Http.IFormFile ImageFile { get; set; }
     }
